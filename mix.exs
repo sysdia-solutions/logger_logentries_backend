03db45dcd12f7640cd13bfc94f5e87f7605059a4
Harvey Ball <me@harveyball.com>
2015-12-20 08:09:06 +0000

    chore(mix.exs): include required fields for hex publish
    Added a number of additional required field dependencies to publish the
    package to Hex.pm

--- mix.exs
+++ mix.exs
@@ -2,19 +2,41 @@ defmodule LoggerLogentriesBackend.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :logger_logentries_backend,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :logger_logentries_backend,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description,
+      package: package,
+      deps: deps
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger]
+    ]
   end
 
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    A Logger backend to support the Logentries service
+    (logentries.com) TCP input log mechanism
+    """
+  end
+
+  defp package do
+    [
+      files: ["config", "lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Sysdia Solutions"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sysdia-solutions/logger_logentries_backend"}
+    ]
   end
 end
